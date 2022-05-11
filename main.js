ab70945aceca4440788bc8028e75f3b42889173c
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-05-11 07:38:26 +0300

    block mining
    Implemented basic proof of work functionality

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ class Block {
     this.data = data;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
+    this.nonce = 0;
   }
 
   calculateHash() {
@@ -14,14 +15,27 @@ class Block {
       this.index +
         this.previousHash +
         this.timestamp +
-        JSON.stringify(this.data)
+        JSON.stringify(this.data) +
+        this.nonce
     ).toString();
+  }
+
+  mineBlock(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+
+    console.log(`Block mined: ${this.hash}`);
   }
 }
 
 class Blockchain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 3;
   }
 
   createGenesisBlock() {
@@ -34,7 +48,7 @@ class Blockchain {
 
   addBlock(newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
   }
 
@@ -57,8 +71,13 @@ class Blockchain {
 }
 
 const myCoin = new Blockchain();
+
+console.log("mining block 1");
 myCoin.addBlock(new Block(1, new Date(), { amount: 10 }));
+console.log("mining block 2");
 myCoin.addBlock(new Block(2, new Date(), { amount: 15 }));
 
-console.log(JSON.stringify(myCoin, null, 4));
+// console.log(JSON.stringify(myCoin, null, 4));
+// console.log(myCoin);
+
 console.log("is blockchain valid? : " + myCoin.isChainValid());
