7ddfb21d908aebf511a495e59ec5a76e276732ea
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-05-11 06:46:10 +0300

    Created base blockchain classes

---
+++ README.md
@@ -0,0 +1,1 @@
+### A simple blockchain in Javascript. For educational purposes only.
---
+++ main.js
@@ -0,0 +1,64 @@
+import SHA256 from "crypto-js/sha256.js";
+
+class Block {
+  constructor(index, timestamp, data, previousHash = "") {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+  }
+
+  calculateHash() {
+    return SHA256(
+      this.index +
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.data)
+    ).toString();
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, new Date(), "Genesis Block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+const myCoin = new Blockchain();
+myCoin.addBlock(new Block(1, new Date(), { amount: 10 }));
+myCoin.addBlock(new Block(2, new Date(), { amount: 15 }));
+
+console.log(JSON.stringify(myCoin, null, 4));
+console.log("is blockchain valid? : " + myCoin.isChainValid());
